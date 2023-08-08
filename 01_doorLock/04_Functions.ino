082bdf28b94585ed0e5188745c531ab77f69fb5a
Sithija Vihanga Ranaraja <sithija.vihanga28@gmail.com>
2023-08-08 15:03:53 +0530

    Delete 01_doorLock directory

--- 01_doorLock/04_Functions.ino
+++
@@ -1,8 +0,0 @@
-void printToOled(String text){
-      oled.clearDisplay(); // clear display
-      oled.setTextSize(2);          // text size
-      oled.setTextColor(WHITE);     // text color
-      oled.setCursor(10, 30);        // position to display
-      oled.println(text); // text to display
-      oled.display();
-}
